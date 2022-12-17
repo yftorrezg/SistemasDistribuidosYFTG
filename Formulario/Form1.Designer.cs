e3c5c69eb81c9583eba6fac6f2fbd6729f8f251e
Fernando Torrez <Fer.torrez@outlook.es>
2022-12-17 16:09:57 -0400

    segundo commit p3 y formulario

---
+++ Formulario/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMoneda = new System.Windows.Forms.TextBox();
+            this.id = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.nit = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.modalidad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cliente = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 512);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Emitir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(651, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Moneda:";
+            // 
+            // txtMoneda
+            // 
+            this.txtMoneda.Location = new System.Drawing.Point(790, 147);
+            this.txtMoneda.Name = "txtMoneda";
+            this.txtMoneda.Size = new System.Drawing.Size(100, 31);
+            this.txtMoneda.TabIndex = 2;
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Location = new System.Drawing.Point(102, 187);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(134, 25);
+            this.id.TabIndex = 3;
+            this.id.Text = "ID_Ambiente";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(289, 181);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(100, 31);
+            this.tb_id.TabIndex = 4;
+            this.tb_id.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // nit
+            // 
+            this.nit.Location = new System.Drawing.Point(289, 255);
+            this.nit.Name = "nit";
+            this.nit.Size = new System.Drawing.Size(100, 31);
+            this.nit.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "nit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "modalidad";
+            // 
+            // modalidad
+            // 
+            this.modalidad.Location = new System.Drawing.Point(289, 320);
+            this.modalidad.Name = "modalidad";
+            this.modalidad.Size = new System.Drawing.Size(100, 31);
+            this.modalidad.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 411);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Nombre Cliente";
+            // 
+            // cliente
+            // 
+            this.cliente.Location = new System.Drawing.Point(289, 411);
+            this.cliente.Name = "cliente";
+            this.cliente.Size = new System.Drawing.Size(100, 31);
+            this.cliente.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(167, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "FACTURA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 728);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cliente);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.modalidad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nit);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.txtMoneda);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMoneda;
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.TextBox nit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox modalidad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox cliente;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Formulario/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Formulario.ServiceEjercicio2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceEjercicio1.WebEjercicio1SoapClient pregunta1 = new ServiceEjercicio1.WebEjercicio1SoapClient();
+            ServiceEjercicio2.WebEjercicio2SoapClient pregunta2 = new ServiceEjercicio2.WebEjercicio2SoapClient();
+
+            string fecha = pregunta2.SincronizarFechaHora();
+            string cuf = pregunta2.ObtenerCufd(tb_id.Text.Trim(), modalidad.Text.Trim(), nit.Text.Trim());
+            Factura factura = new Factura();
+            factura.Nit = nit.Text;
+            factura.Fecha = fecha;
+            factura.Cuf = cuf;
+            factura.NombreCliente = cliente.Text.Trim();
+            string facturaEmitida = pregunta2.EmitirFactura(Int32.Parse(tb_id.Text.Trim()), Int32.Parse(nit.Text.Trim()), cuf, Int32.Parse(modalidad.Text.Trim()), factura);
+            MessageBox.Show(facturaEmitida);
+
+            string cotizacion = pregunta1.obtenerCotizacion((txtMoneda.Text.Trim()));
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
